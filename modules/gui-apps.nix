79df5de03742525115bf39686883faa9453a2b4d
yongminari <easyid21c@gmail.com>
2026-04-12 09:57:39 +0900

    terminal issue

--- modules/gui-apps.nix
+++ modules/gui-apps.nix
@@ -14,5 +14,8 @@
     font-family = "D2Coding"
     font-size = 12
     command = ${pkgs.zsh}/bin/zsh
+
+    # SSH 접속 시 터미널 정보(terminfo)를 자동으로 원격지에 복사하여 글자 중복 입력 현상 해결
+    shell-integration-features = ssh-terminfo
   '';
 }
