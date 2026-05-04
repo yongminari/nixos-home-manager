f5e9747fe9b6d33223eabbd64d9f5cdc6efd01aa
yongminari <easyid21c@gmail.com>
2026-05-04 16:18:35 +0900

    add feature to toggle touchpad

--- modules/desktop/niri.nix
+++ modules/desktop/niri.nix
@@ -24,6 +24,51 @@ in
     };
   };
 
+  # 키보드 고정 설정 (Nix 관리)
+  xdg.configFile."niri/keyboard.kdl".text = ''
+    keyboard {
+        xkb {
+            layout "us"
+        }
+        repeat-delay 400
+        repeat-rate 40
+    }
+  '';
+
+  # 터치패드 설정 디렉토리 보장
+  systemd.user.tmpfiles.rules = [
+    "d %h/.config/niri 0755 - - -"
+  ];
+
+  # 터치패드 토글 스크립트
+  xdg.configFile."niri/toggle-touchpad.sh" = {
+    executable = true;
+    text = ''
+      #!/usr/bin/env bash
+      INPUT_CONFIG="$HOME/.config/niri/input.kdl"
+      KEYBOARD_CONFIG="$HOME/.config/niri/keyboard.kdl"
+      
+      # 키보드 설정 읽기 (파일이 없으면 기본값 사용)
+      if [ -f "$KEYBOARD_CONFIG" ]; then
+          KBD_SETTINGS=$(cat "$KEYBOARD_CONFIG")
+      else
+          KBD_SETTINGS="    keyboard { xkb { layout \"us\" }; repeat-delay 400; repeat-rate 40; }"
+      fi
+
+      if grep -q "off" "$INPUT_CONFIG"; then
+          # 활성화 상태로 생성
+          printf "input {\n%s\n    touchpad {\n        tap\n        natural-scroll\n    }\n}\n" "$KBD_SETTINGS" > "$INPUT_CONFIG"
+          notify-send -t 1500 -i input-touchpad "Touchpad" "Enabled"
+      else
+          # 비활성화 상태로 생성
+          printf "input {\n%s\n    touchpad {\n        off\n    }\n}\n" "$KBD_SETTINGS" > "$INPUT_CONFIG"
+          notify-send -t 1500 -i input-touchpad "Touchpad" "Disabled"
+      fi
+      
+      niri msg action load-config-file
+    '';
+  };
+
   # 설정을 text로 생성하여 스케일 값을 주입
   xdg.configFile."niri/config.kdl".text = ''
     // 호스트별 자동 생성된 출력 설정
@@ -32,6 +77,9 @@ in
     }
 
     ${baseConfig}
+    
+    // 통합 입력 설정 포함
+    include "input.kdl"
   '';
 
   xdg.configFile."niri/binds.kdl".source = ./niri/binds.kdl;
