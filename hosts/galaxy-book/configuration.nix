9bc341265df9ed10f73b4910cb356084d02ba798
yongminari <easyid21c@gmail.com>
2026-09-24 19:21:27 +0900

    Limit Galaxy Book battery charging to 90%

--- hosts/galaxy-book/configuration.nix
+++ hosts/galaxy-book/configuration.nix
@@ -15,4 +15,9 @@
 
   # 삼성 갤럭시 북 전용 하드웨어 최적화 활성화
   hardware.samsung-galaxy-book.enable = true;
+
+  # 이 장치의 배터리 충전을 90%로 제한
+  services.udev.extraRules = ''
+    ACTION=="add|change", SUBSYSTEM=="power_supply", KERNEL=="BAT1", TEST=="charge_control_end_threshold", ATTR{charge_control_end_threshold}!="90", ATTR{charge_control_end_threshold}="90"
+  '';
 }
